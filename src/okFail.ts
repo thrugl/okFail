eaaa321a00108de6da7d96bca84347aa1f28e9a8
fitravel <doddi@fitravel.is>
2020-10-27 14:49:16 +0000

    1.6.1

--- src/okFail.ts
+++ src/okFail.ts
@@ -1,8 +1,7 @@
-import isOkFail from './isOkFail'
 import OkFailObject from './OkFailObject'
 
 export const okFail = (ok: boolean, data: any, error?: any) => {
-	const payload = isOkFail(data) ? data.data : data
+	const payload = data?.data ?? data
 	return new OkFailObject<typeof data>(ok, payload, error)
 }
 
